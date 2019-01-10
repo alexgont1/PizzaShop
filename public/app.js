889f9761b24e14753ef85b4b96ff54e6e53c6bb4
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-10 15:30:33 -0600

    add comments for JS function

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('bbb');//equal: x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; //string to int and +1
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x);//hh['bbb'] = x
 
 	alert(x);
 }
