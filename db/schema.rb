5647651beabb996198a68e6463177f0d9e1b32a9
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-09 20:15:48 -0600

    add images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_012329) do
+ActiveRecord::Schema.define(version: 2019_01_10_014430) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
