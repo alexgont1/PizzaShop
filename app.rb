4641d4966040fc07daa4cfc6dff968270278c44f
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-13 12:57:14 -0600

    update views

--- app.rb
+++ app.rb
@@ -21,36 +21,39 @@ get '/about' do
 end
 
 post '/place_order' do
-	@order = Order.create params[:order]
-	erb :order_placed
+  @order = Order.create params[:order]
+  erb :order_placed
 end
 
 post '/cart' do
-	@orders_input = params[:orders]
-	@items = parse_orders_input @orders_input
+  @orders_input = params[:orders_input]
+  @items = parse_orders_input @orders_input
 
-	@items.each do |item|
-		#id, total
-	 	item[0] = Product.find(item[0]) 
-	end
+  @items.each do |item|
+    # id, cnt
+    item[0] = Product.find(item[0])
+  end
 
 	erb :cart
 end
 
 def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
 
-	s1 = orders_input.split(/,/)
+  arr = []
 
-	arr = []
-	s1.each do |x|
-		s2 = x.split(/=/)
+  s1.each do |x|
+    s2 = x.split(/\=/)
 
-		s3 = s2[0].split(/_/)
+    s3 = s2[0].split(/_/)
 
-		id = s3[1] #key
-		cnt = s2[1] #value
-		arr2 = [id, cnt]
-		arr.push arr2
-	end
-	return arr
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
 end
